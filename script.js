7df6256f452a44fcda81379e9b61d4cbeb42a6d4
Ray Tomlin <38268973+Spr18JSRT@users.noreply.github.com>
2018-04-24 10:41:08 -0400

    Updated Comments
    I've updated comments for this section.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-# This file is for the JavaScript code
+/*
+Authors with initials: 
+                     :
+                     :
+                     :
+                     :
+                     : Ray Tomlin - RT  Divs & validate / Paragraph / Make sure all code, on all files, is commented accordingly
+                     :
+                     :
+                     
+Last updated Date:   4/24/18                     
+*/
